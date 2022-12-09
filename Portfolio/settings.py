05e4344891ae82a3d20faf065694a075b591ce81
FarjallahAhmed <ahmed.farjallah@esprit.tn>
2022-12-09 10:32:36 +0100

    removing secret key

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)=t3&pgqv+vd3+&m70_50$l7@xa8$y-qlm@*1wecoxtf$n_umz'
-
+#SECRET_KEY = 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
