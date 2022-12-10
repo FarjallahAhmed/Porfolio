68e81798d8fb3b1294263bdacf64eb03b7155c79
FarjallahAhmed <ahmed.farjallah@esprit.tn>
2022-12-10 11:38:33 +0100

    start the about me app

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'about.app.AboutConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
